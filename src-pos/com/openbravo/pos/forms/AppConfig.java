c97b243bc48919782c4656f82b5eef79890abc74
Heshan <heshan@heidisoft.com>
2010-11-25 20:32:33 +0530

    Adding database config to mysql.

--- src-pos/com/openbravo/pos/forms/AppConfig.java
+++ src-pos/com/openbravo/pos/forms/AppConfig.java
@@ -126,11 +126,11 @@ public class AppConfig implements AppProperties {
         String dirname = System.getProperty("dirname.path");
         dirname = dirname == null ? "./" : dirname;
 
-        m_propsconfig.setProperty("db.driverlib", new File(new File(dirname), "lib/derby.jar").getAbsolutePath());
-        m_propsconfig.setProperty("db.driver", "org.apache.derby.jdbc.EmbeddedDriver");
-        m_propsconfig.setProperty("db.URL", "jdbc:derby:" + new File(new File(System.getProperty("user.home")), AppLocal.APP_ID + "-database").getAbsolutePath() + ";create=true");
-        m_propsconfig.setProperty("db.user", "");
-        m_propsconfig.setProperty("db.password", "");
+//        m_propsconfig.setProperty("db.driverlib", new File(new File(dirname), "lib/derby.jar").getAbsolutePath());
+//        m_propsconfig.setProperty("db.driver", "org.apache.derby.jdbc.EmbeddedDriver");
+//        m_propsconfig.setProperty("db.URL", "jdbc:derby:" + new File(new File(System.getProperty("user.home")), AppLocal.APP_ID + "-database").getAbsolutePath() + ";create=true");
+//        m_propsconfig.setProperty("db.user", "");
+//        m_propsconfig.setProperty("db.password", "");
 
 //        m_propsconfig.setProperty("db.driverlib", new File(new File(dirname), "lib/hsqldb.jar").getAbsolutePath());
 //        m_propsconfig.setProperty("db.driver", "org.hsqldb.jdbcDriver");
@@ -138,11 +138,11 @@ public class AppConfig implements AppProperties {
 //        m_propsconfig.setProperty("db.user", "sa");
 //        m_propsconfig.setProperty("db.password", "");
 
-//        m_propsconfig.setProperty("db.driverlib", new File(new File(dirname), "lib/mysql-connector-java-5.1.13-bin.jar").getAbsolutePath());
-//        m_propsconfig.setProperty("db.driver", "com.mysql.jdbc.Driver");
-//        m_propsconfig.setProperty("db.URL", "jdbc:mysql://localhost:3306/opb_new");
-//        m_propsconfig.setProperty("db.user", "root");
-//        m_propsconfig.setProperty("db.password", "");
+        m_propsconfig.setProperty("db.driverlib", new File(new File(dirname), "lib/mysql-connector-java-5.1.13-bin.jar").getAbsolutePath());
+        m_propsconfig.setProperty("db.driver", "com.mysql.jdbc.Driver");
+        m_propsconfig.setProperty("db.URL", "jdbc:mysql://localhost:3306/database");
+        m_propsconfig.setProperty("db.user", "root");
+        m_propsconfig.setProperty("db.password", "");
 
 //        m_propsconfig.setProperty("db.driver", "org.postgresql.Driver");
 //        m_propsconfig.setProperty("db.URL", "jdbc:postgresql://localhost:5432/database");
